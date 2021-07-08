3bdce42b6db606110daeaa8481d24e5efb9177d6
Nate Denys <nate_denys@yahoo.com>
2021-07-08 14:25:57 -0700

    upload of completed assignment ready for deploy to pages

---
+++ precache-manifest.9ba1ca24cfc3476396aab1db786b9618.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b859b734df34df1938",
+    "url": "/double_clicker/css/chunk-vendors.e409c32a.css"
+  },
+  {
+    "revision": "004b8aeac37e3fc315decef3158830c6",
+    "url": "/double_clicker/index.html"
+  },
+  {
+    "revision": "abb39153ce29d2af337f",
+    "url": "/double_clicker/js/app.586067d2.js"
+  },
+  {
+    "revision": "94b859b734df34df1938",
+    "url": "/double_clicker/js/chunk-vendors.a00a00e3.js"
+  },
+  {
+    "revision": "3e6125ed8b98c5715ef696f5fdc724f7",
+    "url": "/double_clicker/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/double_clicker/robots.txt"
+  }
+]);
